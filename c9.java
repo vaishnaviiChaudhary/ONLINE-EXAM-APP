dd547a0ec55fa78ec5a23c1ba2dc3b132e85bd96
Vaishnavi Chaudhary <114181304+vaishnaviiChaudhary@users.noreply.github.com>
2022-10-01 18:36:27 +0530

    Create c9.java

---
+++ c9.java
@@ -0,0 +1,110 @@
+package miniProject;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class c9 extends JFrame implements ActionListener {
+
+    JLabel l1,l2;
+
+    JRadioButton a,b,c,d;
+
+    JButton next,prev;
+    int rdX=75,rdW=320,rdH=50;
+
+    ButtonGroup opt;
+
+    c9() {
+        setSize(1000, 700);
+
+        l1 = new JLabel("C++  BASICS");
+        l1.setBounds(400, 20, 1000, 100);
+        l1.setFont(new Font("Tahoma", Font.BOLD, 30));
+        add(l1);
+
+        l2 = new JLabel("9. What is the benefit of c++ input and output over c input and output? ");
+        l2.setBounds(30, 130, 1000, 100);
+        l2.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        add(l2);
+
+        opt = new ButtonGroup();
+
+        a = new JRadioButton("a) Both Type safety & Exception");
+        a.setBounds(rdX, 270, rdW, rdH);
+        opt.add(a);
+        add(a);
+
+        b = new JRadioButton("b) Sequence container");
+        b.setBounds(rdX, 340, rdW, rdH);
+        opt.add(b);
+        add(b);
+
+        c = new JRadioButton("c) Exception");
+        c.setBounds(rdX, 410, rdW, rdH);
+        opt.add(c);
+        add(c);
+
+        d = new JRadioButton("d) Type safety");
+        d.setBounds(rdX, 490, rdW, rdH);
+        opt.add(d);
+        add(d);
+
+        next = new JButton("Next");
+        next.setBounds(770,550,100,40);
+        next.addActionListener(this);
+        add(next);
+
+        prev = new JButton("Previous");
+        prev.setBounds(650,550,100,40);
+        prev.addActionListener(this);
+        add(prev);
+
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent ae)
+    {
+        String option = null;
+        if(a.isSelected())
+        {
+            option = "a) Both Type safety & Exception";
+        }
+        else if(b.isSelected())
+        {
+            option = "b) Sequence container";
+        }
+        else if (c.isSelected())
+        {
+            option = "c) Exception";
+        }
+        else if(d.isSelected())
+        {
+            option = "d) Type safety";
+        }
+
+        if(ae.getSource()==next)
+        {
+            new c10();
+            this.setVisible(false);
+        }
+        if(ae.getSource()==prev)
+        {
+            new c8();
+            this.setVisible(false);
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        new c9();
+    }
+
+
+}
